6ff8949138c090eebe440a36df3e09ca5b3ebc68
Sara Fernández Arias <uo269546@uniovi.es>
2022-04-30 18:30:56 +0200

    RegExp for checking fields

--- node/routes/users.js
+++ node/routes/users.js
@@ -1,4 +1,7 @@
 module.exports = function (app, usersRepository) {
+    const emailRegexp = new RegExp("\\w*\\@\\w*\\.\\w*");
+    const nombreYapellidosRegExp = new RegExp("[a-zA-Z]+('-'|' '[a-zA-Z])*");
+    const pswdRegExp = new RegExp("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$")//passwords must have at least eight characters, with at least one letter and one number.
     app.get('/users', function (req, res) {
         res.send('lista de usuarios')
         ;
@@ -21,33 +24,43 @@ module.exports = function (app, usersRepository) {
      *     si el usuario NO es administrador->se le redirige a la vista “Ver listado de usuarios de la red social”
      */
     app.post('/users/login', function (req, res) {
-        let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
-            .update(req.body.password).digest('hex');
-        let filter = {
-            email: req.body.email,
-            password: securePassword
-        }
-        let options = {};
-
-        usersRepository.findUser(filter, options).then(user => {
-            if (user == null) {
-                req.session.user = null;
-
-                res.redirect("/users/login" +
-                    "?message=Email o password incorrecto" +
-                    "&messageType=alert-danger ");
-
-            } else {
-                req.session.user = user.email;
-                res.redirect("/publications");
-            }
-        }).catch(error => {
-            req.session.user = null;
+        if (req.body.email == null || req.body.password == null) {
             res.redirect("/users/login" +
-                "?message=Se ha producido un error al buscar el usuario" +
+                "?message=Debes rellenar todos los campos para iniciar sesión." +
                 "&messageType=alert-danger ");
-        })
-        ;
+        } else if (!req.body.email.match(emailRegexp)) {
+            res.redirect("/users/login" +
+                "?message=El formato del email es incorrecto." +
+                "&messageType=alert-danger ");
+        } else {
+            let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+                .update(req.body.password).digest('hex');
+            let filter = {
+                email: req.body.email,
+                password: securePassword
+            }
+            let options = {};
+
+            usersRepository.findUser(filter, options).then(user => {
+                if (user == null) {
+                    req.session.user = null;
+
+                    res.redirect("/users/login" +
+                        "?message=Email o password incorrecto" +
+                        "&messageType=alert-danger ");
+
+                } else {
+                    req.session.user = user.email;
+                    res.redirect("/publications");
+                }
+            }).catch(error => {
+                req.session.user = null;
+                res.redirect("/users/login" +
+                    "?message=Se ha producido un error al buscar el usuario" +
+                    "&messageType=alert-danger ");
+            })
+            ;
+        }
     });
     /**
      * Método mediante el cual se envían los datos necesarios para registrarse en la aplicación.
@@ -59,16 +72,33 @@ module.exports = function (app, usersRepository) {
      * y la contraseña ha de contener al menos letras y números.
      */
     app.post('/users/signup', function (req, res) {
-        let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
-            .update(req.body.password).digest('hex');
-        let user = {
-            email: req.body.email,
-            password: securePassword
+        if (req.body.email == null || req.body.password == null) {
+            res.redirect("/users/signup" +
+                "?message=Debes rellenar todos los campos para registrarte como usuario." +
+                "&messageType=alert-danger ");
+        } else if (!req.body.email.match(emailRegexp)) {
+            res.redirect("/users/signup" +
+                "?message=El formato del email es incorrecto." +
+                "&messageType=alert-danger ");
+        } else if (!req.body.nombre.match(nombreYapellidosRegExp) || !req.body.apellidos.match(nombreYapellidosRegExp)) {
+
+            res.redirect("/users/signup" +
+                "?message=Los campos de nombre y apellidos solamente aceptan letras , espacios o guiones." +
+                "&messageType=alert-danger ");
+        } else {
+            let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+                .update(req.body.password).digest('hex');
+            let user = {
+                email: req.body.email,
+                name:req.body.name,
+                surname:req.body.surname,
+                password: securePassword
+            }
+            usersRepository.insertUser(user).then(userId => {
+                res.redirect("/users/login" + '?message=Nuevo usuario registrado.' + "&messageType=alert-info");
+            }).catch(error => {
+                res.redirect("/users/signup" + '?message=Se ha producido un error al registrar el usuario.' + "&messageType=alert-danger");
+            });
         }
-        usersRepository.insertUser(user).then(userId => {
-            res.redirect("/users/login" + '?message=Nuevo usuario registrado.' + "&messageType=alert-info");
-        }).catch(error => {
-            res.redirect("/users/signup" + '?message=Se ha producido un error al registrar el usuario.' + "&messageType=alert-danger");
-        });
     });
 }
