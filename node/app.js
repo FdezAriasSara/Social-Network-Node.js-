2c6ebcda9648557e0d5b5ff69d9cfc30fe85a09f
Martin Beltran <uo276244@uniovi.es>
2022-05-06 10:47:38 +0200

    IndexRouter

--- node/app.js
+++ node/app.js
@@ -128,7 +128,7 @@ require("./routes/publications.js")(app, usersRepository, publicationsRepository
 require("./routes/api/chatAPI")(app, publicationsRepository, usersRepository, messagesRepository);
 
 
-////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -139,7 +139,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+var indexRouter = require("./routes/index")
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
