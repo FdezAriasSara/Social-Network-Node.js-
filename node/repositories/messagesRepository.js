9e723406adc761314cff40eb04f0391f1ebca06c
Sebastian L.H <66498000+sebaslh01@users.noreply.github.com>
2022-05-08 21:55:41 +0200

    Adding deleteMessages

--- node/repositories/messagesRepository.js
+++ node/repositories/messagesRepository.js
@@ -46,5 +46,18 @@ module.exports = {
 
         }
 
+    },
+    deleteMessages:async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("redsocial");
+            const collectionName = 'messages';
+            const messagesCollection = database.collection(collectionName);
+
+            const result = await messagesCollection.deleteMany(filter, options);
+            return result;
+        } catch (error) {
+            throw (error);
+        }
     }
 }
